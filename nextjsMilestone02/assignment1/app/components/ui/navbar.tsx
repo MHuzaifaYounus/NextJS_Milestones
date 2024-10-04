bd1b31fadf2509cb052b1ba81c655c827b3ad4c3
Muhammad Huzaifa <huzaifayounus360@gmail.com>
2024-10-05 00:27:37 +0500

    adding hamburgur

--- nextjsMilestone02/assignment1/app/components/ui/navbar.tsx
+++ nextjsMilestone02/assignment1/app/components/ui/navbar.tsx
@@ -1,15 +1,35 @@
+"use client"
 import Link from "next/link"
+import { Menu } from "lucide-react"
+import { useEffect, useState } from "react";
 export default function NavBar() {
+  const [ShowNavbar, setShowNavbar] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (screen.width <= 700) {
+      setShowNavbar(false)
+    }
+  }, [])
+  function menuClickHandler() {
+    setShowNavbar(!ShowNavbar)
+
+  }
+  function navbarhideHandler() {
+    if(screen.width <= 700){
+      setShowNavbar(false)
+    }
+  }
   return (
     <div className="header">
       <div className="logo"><h1><span>P</span>ixel<span>P</span>ages<span>.</span></h1></div>
       <div className="nav">
-        <ul>
+        <Menu className="menuIcon" onClick={menuClickHandler} />
+        {ShowNavbar && <ul onClick={navbarhideHandler}>
           <li><Link href={"/"}>Home</Link></li>
           <li><Link href={"/services"}>Services</Link></li>
           <li><Link href={"/about"}>About</Link></li>
-          <li><Link href={"/contact"}>Contact us</Link></li>
-        </ul>
+          <li><Link href={"/contact"}>Contact</Link></li>
+        </ul>}
       </div>
     </div>
   );
